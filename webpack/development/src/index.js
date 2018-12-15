713673cf4a13c4f1f53a8f4692982abd44e5ac15
saif <saifullah.khan@99array.com>
2018-12-15 19:14:55 +0530

    Development part completed

---
+++ webpack/development/src/index.js
@@ -0,0 +1,19 @@
+// import _ from 'lodash';
+import printMe from "./print"
+
+function component() {
+    let element = document.createElement('div');
+    var btn = document.createElement('button');
+
+    // Lodash, currently included via a script, is required for this line to work
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = "Something good start with little efforts";
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
+
+    return element;
+}
+
+document.body.appendChild(component());
